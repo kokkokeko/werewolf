94e1d70e2fc4eeeea5ffabf2f517db0f1b286e1e
kokkokeko <kk5321384@gmail.com>
2020-10-22 10:06:08 +0900

    Inplement Entry Phase

---
+++ index.js
@@ -0,0 +1,67 @@
+/* サーバーを初期化 */
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+/* Socket.io */
+const io = require('socket.io')(http);
+const gameRoom = io.of('/1001');
+
+/* 準備フェーズ **********************/
+const playerIds = [];
+let players;
+
+// ゲームに参加するクライアントを決める
+app.get('/entry', (req, res) => {  
+  // 五人以上いる場合  
+  if (playerIds.length >= 5) {
+    return res.json({entryResult: 'denied'});
+  }  
+  // 五人以下の場合
+  return res.json({entryResult: 'accepted', room: '1001'});
+});
+
+// 参加者が決まり、通信を始める
+gameRoom.on('connection', socket => {
+  console.log('someone connected');
+  playerIds.push(socket.id);
+  // socket.idの例： '/1001#xe_-8ijoihv7DnfgAAAA'
+
+  if (playerIds.length === 5) {
+    players = initializeGame(playerIds);
+  }
+});
+
+function initializeGame(playerIds) {
+  // 人狼のidxを決める。
+  const werewolf = Math.floor(Math.random() * (4 + 1) );
+  // player情報
+  const players = {};
+
+  playerIds.forEach( (id, idx) => {
+    players[id] = {};    
+    players[id].group = idx === werewolf
+      ? 'werewolf'
+      : 'villagers';
+    players[id].isDead = false;
+  });
+
+  return players;
+}
+
+/* 昼フェーズ **********************/
+
+/* 夜フェーズ **********************/
+
+/* 結果フェーズ **********************/
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
